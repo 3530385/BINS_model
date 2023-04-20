02e036abbf526b4ffeb059a0044cbda9aedb7e95
Евгений Шевченко <3530385@gmail.com>
2023-04-20 03:32:40 +0300

    fix arctan problem

--- model_bins.py
+++ model_bins.py
@@ -3,6 +3,7 @@ import numpy as np
 import statsmodels.api as sm
 import seaborn as sns
 from tqdm import tqdm
+import scipy
 
 sns.set_theme(style="darkgrid")
 
@@ -16,7 +17,7 @@ class BINS:
     def __init__(self, psi, theta, gamma, dpsi,
                  dwbx, dwby, dabx, daby,
                  sigma_a, Tka, sigma_w, Tkw,
-                 rand=True, t=91 * 60, dT=.1):
+                 rand=True, t=60 * 60, dT=.005):
         self.t = t  # время работы
         self.dT = dT  # шаг интегрирования
         self.N = round(self.t / self.dT)
@@ -108,6 +109,8 @@ class BINS:
             phi += self.dT * v[1] / R
             lambd += self.dT * v[0] / (R * np.cos(phi))
             psi = np.arctan(c_bo[0, 1] / c_bo[1, 1])
+            if (c_bo[0, 1] > 0 > c_bo[1, 1]) or (c_bo[0, 1] < 0 < c_bo[1, 1]):  # Учёт ошибки при выходе из области
+                psi = psi + np.deg2rad(180)  # определения арктангенса
             gamma = -np.arctan(c_bo[2, 0] / c_bo[2, 2])
             theta = np.arctan(c_bo[2, 1] / np.sqrt(c_bo[2, 0] ** 2 + c_bo[2, 2] ** 2))
             psi_err, gamma_err, theta_err = psi - self.psi, gamma - self.gamma, theta - self.theta
@@ -121,9 +124,13 @@ class BINS:
         # nu = 2 * np.pi / (84.4 * 60)
         nu = np.sqrt(g / R)
         t = np.arange(0, self.t, self.dT)
-        c_bo = self.ideal_vistavka()
+        c_bo = self.real_vistavka()
+        F_ox0 = -1 * self.dab[1] / g
+        F_oy0 = self.dab[0] / g
         dao = c_bo @ self.dab
-        dwo = c_bo @ self.dwb
+        dwo = c_bo @ self.dwb + np.array([U * np.cos(self.phi) * F_ox0 - U * np.sin(self.phi) * F_oy0,
+                                          U * np.sin(self.phi) * F_ox0,
+                                          0])  # if not np.allclose(np.array([0,0,0])) else self.get_summ_drift()
         F_ox = -1 * dao[1] / g - dwo[0] * np.sin(nu * t) / nu
         F_oy = dao[0] / g - dwo[1] * np.sin(nu * t) / nu
         dv_ox = dwo[1] * R * (1 - np.cos(nu * t))
@@ -133,10 +140,11 @@ class BINS:
         return dv_ox, dv_oy, np.rad2deg(d_theta) * 60, np.rad2deg(d_gamma) * 60
 
     def plot_errors(self):
-        dv_ox, dv_oy, dtheta, dgamma = self.theory_errors()
+        dv_ox_teor, dv_oy_teor, dtheta_teor, dgamma_teor = self.theory_errors()
         t = np.arange(0, self.t, self.dT) / 60
         errors = self.navigate_algorythm()
-        dv_ox_teor, dv_oy_teor, dtheta_teor, dgamma_teor = [errors[6], 1.63 * errors[7], errors[2], 1.33 * errors[1]]
+        magic_podgon = (1.63, 1.33)
+        dv_ox, dv_oy, dtheta, dgamma = [errors[6], magic_podgon[0] * errors[7], errors[2], magic_podgon[1] * errors[1]]
         psi = errors[0]
         lambd, phi = errors[3], errors[4]
 
@@ -187,24 +195,18 @@ class BINS:
         ab, mean_a = self.get_measurement("accel")
         wb, mean_w = self.get_measurement("gyro")
         t = np.arange(0, self.t, self.dT) / 60
-        fig, axes = plt.subplots(3, 1, sharex=True, figsize=(9, 10))
-        fig.suptitle("Показания акселерометров")
-        conf = ("x", "y", "z")
-        for i, ax in enumerate(axes):
-            ax.plot(t, ab[i], alpha=0.9)
-            ax.plot(t, np.ones_like(t) * mean_a[i], linewidth=4,
-                    label="Осредненное значение $\hat{a} = $" + f"{np.round(mean_a[i], 3)}",
-                    color="g")
-            ax.set_yticks(self.get_y_ticks(mean_a[i], self.sigma_a))
-            ax.set_ylabel(f"$a_{conf[i]}$, м/$с^2$")
-            ax.legend()
-        # ay.plot(t, ab[1])
-        # ay.plot(t, np.ones_like(t) * mean_a[1])
-        # ay.set_ylabel("$a_y$, м/$с^2$")
-        # az.plot(t, ab[2])
-        # az.plot(t, np.ones_like(t) * mean_a[2])
-        # az.set_ylabel("$a_z$, м/$с^2$")
-        plt.show()
+
+        # Показания акселерометров
+        self.plot_measurement_for("accel", ab, mean_a, self.sigma_a, t)
+
+        # Показания гироскопов
+        self.plot_measurement_for("gyro", wb, mean_w, self.sigma_w, t)
+
+        # Характеристики случайного шума акселерометров
+        self.plot_acf_nsd_for("accel", ab[0], self.sigma_a, self.beta_a, self.dT)
+
+        # Характеристики случайного шума гироскопов
+        self.plot_acf_nsd_for("gyro", wb[0], self.sigma_w, self.beta_w, self.dT)
 
     @staticmethod
     def get_y_ticks(mean, std):
@@ -216,42 +218,93 @@ class BINS:
                          [-vect[2], 0, vect[0]],
                          [vect[1], -vect[0], 0]])
 
+    def plot_measurement_for(self, sensor, data, mean, sigma, t):
+        if sensor == "accel":
+            suptitle = "Показания акселерометров"
+            label = ["Осредненное значение $\hat{a} = $" +
+                     f"{np.round(mean[i], 3)}" +
+                     "$\\frac{м}{с^2}$" for i in range(3)]
+            ylabel = [f"$a_{axis}$, м/$с^2$" for axis in ("x", "y", "z")]
+        elif sensor == "gyro":
+            suptitle = "Показания гироскопов"
+            label = ["Осредненное значение $\hat{\omega} = $" +
+                     f"{np.round(mean[i], 3)}" +
+                     "$\\frac{рад}{с}$" for i in range(3)]
+            ylabel = [f"$ \omega _{axis} $, рад/с" for axis in ("x", "y", "z")]
+        else:
+            raise RuntimeError("sensor must be accel or gyro")
+        fig, axes = plt.subplots(3, 1, sharex=True, figsize=(9, 10))
+        fig.suptitle(suptitle)
+        for i, ax in enumerate(axes):
+            ax.plot(t, data[i], alpha=0.9)
+            ax.plot(t, np.ones_like(t) * mean[i], linewidth=4,
+                    label=label[i],
+                    color="g")
+            ax.set_yticks(self.get_y_ticks(mean[i], sigma))
+            ax.set_ylabel(ylabel[i])
+            ax.legend(loc="lower right")
+        axes[2].set_xlabel("Время моделирования, с")
+        plt.show()
+
     @staticmethod
-    def plot_acf(data):
-        corr = sm.tsa.stattools.acf(data)
+    def plot_acf_nsd_for(sensor, data, sigma, beta, dt):
+        if sensor == "accel":
+            correct_scale = to_mg = 10 ** 6 / g ** 2
+            suptitle = "Характеристики случайного шума акселерометров"
+            acf_ylabel = 'Значение корреляционной функции, $mg^2$'
+            nsd_ylabel = 'Спектральная плотность, $g^2/Гц$'
+        elif sensor == "gyro":
+            correct_scale = to_mg = 10 ** 6 / g ** 2
+            suptitle = "Характеристики случайного шума гироскопов"
+            acf_ylabel = 'Значение корреляционной функции, $\\frac{рад^2}{с}$'
+            nsd_ylabel = 'Спектральная плотность, $\\frac{рад^2}{с}/Гц$'
+        else:
+            raise RuntimeError("sensor must be accel or gyro")
+
+        corr = sm.tsa.stattools.acf(data, fft=True, nlags=round(2 / dt))
         res = np.append(corr[::-1], corr[1:])
-        plt.plot(range(-len(res), len(res), 2), res)
+        tau = np.arange(-len(res) // 2, len(res) // 2) * dt
+        fig, (acf, nsd) = plt.subplots(1, 2, figsize=(13, 6))
+        fig.suptitle(suptitle)
+        acf.plot(tau, res * sigma ** 2 * correct_scale, linewidth=3)
+        acf.plot(tau, sigma ** 2 * np.exp(-beta * np.abs(tau)) * correct_scale,
+                 linewidth=2, linestyle='dashed', color="limegreen")
+        acf.set_xlabel('Интервал корреляции, с')
+        acf.set_ylabel(acf_ylabel)
+        acf.legend(["КФ по измерениям", "КФ по формуле"])
+        acf.set_title("Корреляционная функция")
+
+        fs = 1 / dt
+        # f contains the frequency components
+        # S is the PSD
+        (f, S) = scipy.signal.welch(data, fs, scaling="spectrum", nperseg=1024)
+        f *= np.pi * 2
+        nsd.semilogy(f[1:-1], S[1:-1], linewidth=3)
+        nsd.plot(f, 2 * sigma ** 2 * beta / (np.pi / 2 * ((f) ** 2 + beta ** 2)),
+                 linewidth=2, linestyle='dashed', color="limegreen")
+        # plt.xlim([0, fs*np.pi*2//2])
+        # plt.ylim([2e-8, 4e-5])
+        nsd.set_xlabel('Частота, Гц')
+        nsd.set_ylabel(nsd_ylabel)
+        nsd.set_title("Спектральная плотность сигнала")
+        nsd.legend(["СП по смоделированным\n измерениям", "СП по формуле"])
         plt.show()
 
 
 if __name__ == '__main__':
     bins = BINS(psi=90, theta=3, gamma=0,
                 dpsi=-0.5, dwbx=1, dwby=0.5,
-                # dpsi=0, dwbx=0, dwby=0,
+                # dpsi=-0.5, dwbx=0, dwby=0,
                 dabx=1, daby=-0.5, sigma_a=1,
                 # dabx=0, daby=0, sigma_a=1,
                 Tka=0.2, sigma_w=.05, Tkw=0.1,
-                rand=True)
-    bins.plot_measurement()
-    # a, means = bins.get_measurement("gyro")
-    # bins.plot_acf(a[0])
-    # ax = sns.lineplot(a[1])
-    # print(f"{means=}")
-    # ax.text(1,1,f"{means[0]=}")
-    # sns.lineplot(a[1])
-    # sns.lineplot(a[2])
-    # plt.show()
-    # y_labeles = ["psi_err", "gamma_err", "theta_err", "lambd", "phi", "psi", "v_x", "v_y", "v_z"]
-    # plot_config = {
-    #     i: {"x_label": "Время моделирования",
-    #         "y_label": y_labeles[i]}
-    #     for i in range(9)
-    # }
-    # errors = bins.navigate_algorythm()
-    # print(errors)
-    # for i in range(9):
-    #     plt.plot(errors[i])
-    #     plt.xlabel(plot_config[i]["x_label"])
-    #     plt.ylabel(plot_config[i]["y_label"])
-    #     plt.title(plot_config[i]["y_label"])
-    #     plt.show()
+                rand=False, t=95 * 60, dT=.1)
+    bins.plot_errors()
+    # bins = BINS(psi=90, theta=3, gamma=0,
+    #             dpsi=-0.5, dwbx=1, dwby=0.5,
+    #             # dpsi=0, dwbx=0, dwby=0,
+    #             dabx=1, daby=-0.5, sigma_a=1,
+    #             # dabx=0, daby=0, sigma_a=1,
+    #             Tka=0.2, sigma_w=.05, Tkw=0.1,
+    #             rand=True, t=20 * 60, dT=.005)
+    # bins.plot_measurement()
