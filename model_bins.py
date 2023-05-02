43ace0a7b09cd873b654c8bb9d083a39d16d7087
Евгений Шевченко <3530385@gmail.com>
2023-05-02 16:44:56 +0300

    add requirements.txt

--- model_bins.py
+++ model_bins.py
@@ -314,22 +314,22 @@ class BINS:
 
 
 if __name__ == '__main__':
-    # bins = BINS(psi=90, theta=-3, gamma=2,
-    #             dpsi=1, dwbx=-2, dwby=1,
-    #             # dpsi=1, dwbx=0, dwby=0,
-    #             # dabx=-2, daby=1, sigma_a=0.5,
-    #             dabx=0, daby=0, sigma_a=0.5,
-    #             Tka=0.2, sigma_w=.05, Tkw=0.1,
-    #             rand=False, t=95 * 60, dT=.1)
-    # bins.plot_errors()
     bins = BINS(psi=90, theta=-3, gamma=2,
                 dpsi=1, dwbx=-2, dwby=1,
                 # dpsi=1, dwbx=0, dwby=0,
-                dabx=-2, daby=1, sigma_a=0.5,
-                # dabx=0, daby=0, sigma_a=0.5,
+                # dabx=-2, daby=1, sigma_a=0.5,
+                dabx=0, daby=0, sigma_a=0.5,
                 Tka=0.2, sigma_w=.05, Tkw=0.1,
-                rand=True, t=5 * 60, dT=.005)
-    bins.plot_measurement()
+                rand=False, t=95 * 60, dT=.1)
     bins.plot_errors()
+    # bins = BINS(psi=90, theta=-3, gamma=2,
+    #             dpsi=1, dwbx=-2, dwby=1,
+    #             # dpsi=1, dwbx=0, dwby=0,
+    #             dabx=-2, daby=1, sigma_a=0.5,
+    #             # dabx=0, daby=0, sigma_a=0.5,
+    #             Tka=0.2, sigma_w=.05, Tkw=0.1,
+    #             rand=True, t=5 * 60, dT=.005)
+    # bins.plot_measurement()
+    # bins.plot_errors()
     # print(bins.ideal_vistavka())
     # print(bins.real_vistavka())
